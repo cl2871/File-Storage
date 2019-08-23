00334cb49fa3765bb992c5707a661e11623dbe6e
Chris <cl2871@nyu.edu>
2019-08-23 17:08:52 -0400

    added delete tests and verify methods for gcp service

--- src/test/java/com/experimentation/filestorage/bucket/GCPBucketStorageServiceImplTest.java
+++ src/test/java/com/experimentation/filestorage/bucket/GCPBucketStorageServiceImplTest.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.internal.verification.VerificationModeFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
@@ -18,6 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {GCPBucketStorageServiceImpl.class})
@@ -36,9 +38,6 @@ public class GCPBucketStorageServiceImplTest {
     private FileStorageUtil fileStorageUtil;
 
     @MockBean
-    private Blob blob;
-
-    @MockBean
     private MultipartFile multipartFile;
 
     // Final classes
@@ -47,10 +46,13 @@ public class GCPBucketStorageServiceImplTest {
     private BlobId blobId;
     private String contentType;
     private byte[] content;
+    private boolean deletedTrue;
+    private boolean deletedFalse;
 
     // Other fields to initialize
     private FileStorageDTO fileStorageDTO;
     private BlobInfo blobInfo;
+    private Blob blob;
 
     @Before
     public void setUp() throws Exception {
@@ -61,7 +63,10 @@ public class GCPBucketStorageServiceImplTest {
         content = "Example text".getBytes();
 
         fileStorageDTO = new FileStorageDTO(fileName, contentType, content);
-        blobInfo = gcpBucketStorageUtil.createBlobInfo(blobId, contentType);
+        blobInfo = Mockito.mock(BlobInfo.class);
+        blob = Mockito.mock(Blob.class);
+        deletedTrue = true;
+        deletedFalse = false;
     }
 
     @Test
@@ -86,12 +91,14 @@ public class GCPBucketStorageServiceImplTest {
         assertThat(fileStorageDTO.getFileName()).isEqualTo(fileName);
         assertThat(fileStorageDTO.getContentType()).isEqualTo(contentType);
         assertThat(fileStorageDTO.getData()).isEqualTo(content);
+        verifyStorageGetIsCalledOnce();
     }
 
     @Test
     public void uploadMultipartFile_shouldCompleteUpload_whenCalledWithBucketNameAndFileNameAndMultipartFile()
             throws IOException {
 
+        // Arrange
         Mockito.doReturn(blobId)
                 .when(gcpBucketStorageUtil).createBlobId(bucketName, fileName);
         Mockito.doReturn(contentType)
@@ -103,10 +110,54 @@ public class GCPBucketStorageServiceImplTest {
         Mockito.doReturn(blob)
                 .when(storage).create(blobInfo, content);
 
+        // Act
         gcpBucketStorageService.uploadMultipartFile(bucketName, fileName, multipartFile);
+
+        // Assert
+        verifyStorageCreateIsCalledOnce();
     }
 
     @Test
-    public void deleteFile() {
+    public void deleteFile_shouldDelete_whenCalledWithFileNameAndFileIsInBucket() {
+
+        // Arrange
+        Mockito.doReturn(blobId)
+                .when(gcpBucketStorageUtil).createBlobId(bucketName, fileName);
+        Mockito.doReturn(deletedTrue)
+                .when(storage).delete(blobId);
+
+        // Act
+        gcpBucketStorageService.deleteFile(bucketName, fileName);
+
+        // Assert
+        verifyStorageDeleteIsCalledOnce();
+    }
+
+    @Test(expected = FileStorageServiceException.class)
+    public void deleteFile_shouldThrowFileStorageServiceException_whenCalledWithFileNameAndFileIsNotInBucket() {
+
+        // Arrange
+        Mockito.doReturn(blobId)
+                .when(gcpBucketStorageUtil).createBlobId(bucketName, fileName);
+        Mockito.doReturn(deletedFalse)
+                .when(storage).delete(blobId);
+
+        // Act
+        gcpBucketStorageService.deleteFile(bucketName, fileName);
+    }
+
+    private void verifyStorageGetIsCalledOnce() {
+        Mockito.verify(storage, VerificationModeFactory.times(1))
+                .get(any(BlobId.class));
+    }
+
+    private void verifyStorageCreateIsCalledOnce() {
+        Mockito.verify(storage, VerificationModeFactory.times(1))
+                .create(any(BlobInfo.class), any(byte[].class));
+    }
+
+    private void verifyStorageDeleteIsCalledOnce() {
+        Mockito.verify(storage, VerificationModeFactory.times(1))
+                .delete(any(BlobId.class));
     }
 }
